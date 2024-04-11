576cc1a27b4806f82eb83da8edac6765f9672a6f
echris10sen <chr15016@byui.edu>
2024-04-10 21:24:31 -0600

    I don't even know anymore

--- src/utils/validation/authValidation.js
+++ src/utils/validation/authValidation.js
@@ -3,14 +3,14 @@ const { body, validationResult } = require('express-validator');
 // Ensure Session
 
 function ensureSession(req, res, next) {
-    // console.log('req.session: ', req.session);
-    // console.log('req.session.id: ', req.session.id);
-    // if (req.session.tokens && req.session.id && req.session) {
-    //     next();
-    // } else {
-    //     res.status(401).send('Unauthorized');
-    // }
-    next();
+    console.log('req.session: ', req.session);
+    console.log('req.session.id: ', req.session.id);
+    if (req.session.tokens && req.session.id && req.session) {
+        next();
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+    // next();
 }
 
 function registerUserValidationRules(req, res, next) {
